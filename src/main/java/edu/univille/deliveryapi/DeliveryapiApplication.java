4006c7b840c1ad8d703e84f8fbc62013681713a3
lkaminskii <lucaskaminskith@gmail.com>
2023-10-15 19:37:21 -0300

    Project created

---
+++ src/main/java/edu/univille/deliveryapi/DeliveryapiApplication.java
@@ -0,0 +1,13 @@
+package edu.univille.deliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryapiApplication.class, args);
+	}
+
+}
