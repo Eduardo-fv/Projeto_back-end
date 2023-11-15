78df0ae9cff3dee982c560dbc641e58ba7ceb9fb
Rafael Vieira <rafaelvieiradatascientist@gmail.com>
2023-11-14 23:59:23 -0300

    adicionado o metodo @GetMapping(/{id}) para obtero delivery pelo id

--- src/main/java/edu/univille/deliveryapi/controller/DeliveryController.java
+++ src/main/java/edu/univille/deliveryapi/controller/DeliveryController.java
@@ -1,18 +1,12 @@
 package edu.univille.deliveryapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import edu.univille.deliveryapi.domain.model.Delivery;
 import edu.univille.deliveryapi.domain.service.DeliveryFinalizationService;
@@ -22,31 +16,35 @@ import edu.univille.deliveryapi.repository.DeliveryRepository;
 @RestController
 @RequestMapping(value = "/deliveries")
 public class DeliveryController {
-	
-	@Autowired
-	private DeliveryRequestService deliveryRequestService;
-	@Autowired
-	private DeliveryFinalizationService deliveryFinalizationService;
-	@Autowired
-	private DeliveryRepository deliveryRepository;
-	
-	@PostMapping
-	@ResponseStatus(code = HttpStatus.CREATED)
-	public Delivery requestDelivery (@RequestBody Delivery delivery) {
-		return deliveryRequestService.requestDelivery(delivery);
-	}
-	
-	@PutMapping("/{id}/finalizated")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void finalize (@PathVariable Long deliveryId) throws Exception {
-		deliveryFinalizationService.finalizeDelivery(deliveryId);
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<Delivery>> getAllDeliveries () {
-		List<Delivery> list = deliveryRepository.findAll();
-		
-		return ResponseEntity.ok(list);
-	}
 
+    @Autowired
+    private DeliveryRequestService deliveryRequestService;
+    @Autowired
+    private DeliveryFinalizationService deliveryFinalizationService;
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Delivery requestDelivery(@RequestBody Delivery delivery) {
+        return deliveryRequestService.requestDelivery(delivery);
+    }
+
+    @PutMapping("/{id}/finalizated")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void finalize(@PathVariable Long deliveryId) throws Exception {
+        deliveryFinalizationService.finalizeDelivery(deliveryId);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Delivery>> getAllDeliveries() {
+        List<Delivery> list = deliveryRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Delivery> getDeliveryById(@PathVariable Long id) {
+        Optional<Delivery> delivery = deliveryRepository.findById(id);
+        return delivery.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
